e0d9ded9b8b5c2bacf06f93ba11674c2c343ebee
Your Name <you@example.com>
2024-06-27 00:04:49 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-let randomNumber = Math.floor((Math.random()*10)+1);
-console.log(randomNumber);
+
+//console.log(randomNumber);
+document.getElementById("btn").addEventListener("click",function(){
+    let randomNumber = Math.floor((Math.random()*10)+1);
+let gussedN= parseInt(document.getElementById("gNumber").value)
+if(randomNumber==gussedN){
+    resultstext= "Correct";
+}else if(randomNumber<gussedN){
+    resultstext= "too high";
+} else if(randomNumber>gussedN){
+   resultstext= "too low";
+}
+document.getElementById("result").textContent = resultstext;
+});
